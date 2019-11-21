e5eb8da47489dfe20bcca40a73cbed00b0ae81c7
guidoctor <43601914+guidoctor@users.noreply.github.com>
2019-11-21 00:27:10 -0500

    initial upload of code shown off in class with remote control moved to forever loop from while

--- main.ts
+++ main.ts
@@ -1,7 +1,31 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    if (radio_group == 9) {
+        radio_group = 0
+    } else {
+        radio_group += 1
+    }
+    remote.setGroup(radio_group)
+})
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == control.deviceSerialNumber()) {
+        is_master = true
+    } else {
+        is_master = false
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
+let is_master = false
+let radio_group = 0
+radio.setTransmitSerialNumber(true)
+radio_group = 0
+is_master = false
+basic.forever(function () {
+    remote.remoteControl()
+    if (is_master) {
+        basic.showString("M")
+    } else {
+        basic.showNumber(radio_group)
+    }
+})
